8decb6d93e9aaac915e7c082c7dd961c5fe84087
Felix Kunaver <felix.kunaver@gmail.com>
2023-12-13 16:15:22 +0100

    MongoFaker

---
+++ DB/DataFaker.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timedelta
+from faker import Faker
+import random
+
+class DataFaker():
+    def __init__(self):
+        self.fake = Faker()
+
+        self.numSites = 5
+        self.numFormats = 10
+        self.numSessions = 10000
+        self.numTournaments = 100
+        self.formatsPerTournament = 2
+        self.tournamentsPerSession = 5
+        self.numTransactions = 10
+
+        self.si_sites_data = [(self.fake.word(),) for _ in range(self.numSites)]
+        self.f_formats_data = [(self.fake.word(),) for _ in range(self.numFormats)]
+
+        current_date = datetime.now().date()
+        self.s_sessions_data = [(date, random.randint(1, 14)) for date in self.generate_unique_dates(current_date, self.numSessions)]
+
+        self.t_tournaments_data = [
+            (self.fake.word(), random.randint(1, 100), random.randint(0, 10000), random.randint(1, self.numSites))
+            for _ in range(self.numTournaments)
+        ]
+
+        formats_shuffled = random.sample(range(1, self.numFormats + 1), self.numFormats)
+
+        self.tournament_formats_mapping = {
+            x + 1: self.generate_random_number_of_items(formats_shuffled, self.formatsPerTournament)
+            for x in range(self.numTournaments)
+        }
+
+        tournaments_shuffled = random.sample(range(1, self.numTournaments + 1), self.numTournaments)
+        self.session_tournaments_mapping = {
+            x + 1: self.generate_random_number_of_items(tournaments_shuffled, self.tournamentsPerSession)
+            for x in range(self.numSessions)
+        }
+
+        self.tr_transactions_data = [
+            (random.randint(1, self.numSites), random.randint(-1000, 1000), date)
+            for date in self.generate_unique_dates(current_date, self.numTransactions)
+        ]
+
+    def generate_unique_dates(self, end_date, num_dates):
+        date_set = set()
+        dates = []
+        while len(dates) < num_dates:
+            current_date = self.fake.date_between_dates(datetime.now().date() - timedelta(days=30 * 365), end_date)
+            if current_date not in date_set:
+                date_set.add(current_date)
+                dates.append(current_date)
+        return dates
+
+    def generate_random_number_of_items(self, items, max_count):
+        count = random.randint(1, max_count)
+        return random.sample(items, min(count, len(items)))
+
+
